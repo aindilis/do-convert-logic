7904ecffeabe7173dac80f126b0b34631ebb68b1
Andrew John Dougherty <adougher9@gmail.com>
2022-08-11 08:30:33 -0400

    Added sample conversion entry, data-git and scripts dirs, to.do file and update.sh sample conversion.

---
+++ data-git/___to_do.pl
@@ -0,0 +1,17 @@
+hasSourceFile('../to.do').
+hasLastParsedTimeStamp('../to.do',[['-',['-',2022,08],11],[':',[':',08,28],16]]).
+
+'now we want to go ahead and work on figuring out which of the four cases entries pertain to:'().
+.
+'question, what happens when it reverts to a previous update? i.e.'('_SINGLEQUOTE_hi_SINGLEQUOTE_,a','_SINGLEQUOTE_ho_SINGLEQUOTE_,b','_SINGLEQUOTE_hi_SINGLEQUOTE_,c','.').
+'what does the b in'('a,b','correspond to if we don_SINGLEQUOTE_t have revisions? would it be last modified times?').
+'what happens if we move or delete a file, or something to do with symlinks or hardlinks?'().
+'what happens if we move an entry between files?'().
+'what about recurrent things like _SINGLEQUOTE_take out the trash_SINGLEQUOTE_?'().
+'how should we save files?'().
+'should we save the first parse, and then incremental changes? i.e.'().
+'should we save them all in a git repo, and just checkout the various revisions as needed?'().
+'how does this mesh with qlf?'().
+'what about using persistency: https://www.swi-prolog.org/pldoc/doc/_swi_/library/persistency.pl'().
+'or mysql?: https://github.com/aindilis/data-integration'().
+'where do we store the updatedirectly'('and depends/2','facts?').
---
+++ scripts/update.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# run from do-convert-logic/scripts/ dir
+
+do-convert-parsecheck-and-convert-to-prolog.pl -f ../to.do -d ../data-git
